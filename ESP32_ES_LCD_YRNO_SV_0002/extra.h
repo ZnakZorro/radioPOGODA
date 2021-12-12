e2f007557dde4baf01b4588380b00acf633769fe
Znak Zorro <ZnakZorro@users.noreply.github.com>
2021-12-12 13:41:50 +0100

    Add files via upload

---
+++ ESP32_ES_LCD_YRNO_SV_0002/extra.h
@@ -0,0 +1,53 @@
+/*
+String getValue(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+*/
+/*
+String getValue(String data,  int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)=='\n' || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+*/
+
+
+
+
+struct sQQQ  {
+  int8_t l;  
+  int8_t m;
+  int8_t h;
+};
+
+
+sQQQ qqq[5]={
+    { -2, 4, -2},
+    { 6, -2, 6},
+    { 6,-6, 6},
+    { 6,-9, 6},
+    { 0,0,0}
+};
